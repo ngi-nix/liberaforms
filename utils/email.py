5f55a1892e817afff1a644a39306f19011192d9b
buttle <chris@gatopelao.org>
2020-03-23 11:41:47 +0100

    Added utils directory

---
+++ utils/email.py
@@ -0,0 +1,133 @@
+"""
+“Copyright 2019 La Coordinadora d’Entitats per la Lleialtat Santsenca”
+
+This file is part of GNGforms.
+
+GNGforms is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+from flask import g, flash, request
+from flask_babel import gettext
+from GNGforms import app
+from GNGforms.models import Site
+import smtplib, ssl, socket
+from threading import Thread
+
+def createSmtpObj():
+    config=g.site.smtpConfig
+    try:
+        if config["encryption"] == "SSL":
+            server = smtplib.SMTP_SSL(config["host"], port=config["port"], timeout=2)
+            server.login(config["user"], config["password"])
+            
+        elif config["encryption"] == "STARTTLS":
+            server = smtplib.SMTP_SSL(config["host"], port=config["port"], timeout=2)
+            context = ssl.create_default_context()
+            server.starttls(context=context)
+            server.login(config["user"], config["password"])
+            
+        else:
+            server = smtplib.SMTP(config["host"], port=config["port"])
+            if config["user"] and config["password"]:
+                server.login(config["user"], config["password"])
+        
+        return server
+    except socket.error as e:
+        if g.isAdmin:
+            flash(str(e), 'error')
+        return False        
+
+
+def sendMail(email, message):
+    server = createSmtpObj()
+    if server:
+        try:
+            header='To: ' + email + '\n' + 'From: ' + g.site.smtpConfig["noreplyAddress"] + '\n'
+            message=header + message
+            server.sendmail(g.site.smtpConfig["noreplyAddress"], email, message.encode('utf-8'))         
+            return True
+        except Exception as e:
+            if g.isAdmin:
+                flash(str(e) , 'error')
+    return False
+
+
+def sendConfirmEmail(user, newEmail=None):
+    link="%suser/validate-email/%s" % (g.site.host_url, user.token['token'])
+    message=gettext("Hello %s\n\nPlease confirm your email\n\n%s") % (user.username, link)
+    message = 'Subject: {}\n\n{}'.format(gettext("GNGforms. Confirm email"), message)
+    if newEmail:
+        return sendMail(newEmail, message)
+    else:
+        return sendMail(user.email, message)
+
+
+def sendInvite(invite):
+    site=Site.find(hostname=invite.hostname)
+    link="%suser/new/%s" % (site.host_url, invite.token['token'])
+    message="%s\n\n%s" % (invite.message, link)
+    message='Subject: {}\n\n{}'.format(gettext("GNGforms. Invitation to %s" % site.hostname), message)
+    
+    return sendMail(invite.email, message)
+    
+
+def sendRecoverPassword(user):
+    link="%ssite/recover-password/%s" % (g.site.host_url, user.token['token'])
+    message=gettext("Please use this link to recover your password")
+    message="%s\n\n%s" % (message, link)
+    message='Subject: {}\n\n{}'.format(gettext("GNGforms. Recover password"), message)
+    
+    return sendMail(user.email, message)
+
+
+def sendNewFormEntryNotification(emails, entry, slug):
+    message=gettext("New form entry in %s at %s\n" % (slug, g.site.hostname))
+    for data in entry:
+        message="%s\n%s: %s" % (message, data[0], data[1])
+    message="%s\n" % message
+
+    message='Subject: {}\n\n{}'.format(gettext("GNGforms. New form entry"), message)
+    for email in emails:
+        sendMail(email, message)
+
+
+def sendExpiredFormNotification(editorEmails, form):
+    message=gettext("The form '%s' has expired at %s" % (form.slug, g.site.hostname))
+    message='Subject: {}\n\n{}'.format(gettext("GNGforms. A form has expired"), message)
+    
+    for email in editorEmails:
+        sendMail(email, message)
+    
+
+def sendNewFormNotification(adminEmails, form):
+    message=gettext("New form '%s' created at %s" % (form.slug, g.site.hostname))
+    message='Subject: {}\n\n{}'.format(gettext("GNGforms. New form notification"), message)
+    
+    for email in adminEmails:
+        sendMail(email, message)
+
+
+def sendNewUserNotification(adminEmails, username):
+    message=gettext("New user '%s' created at %s" % (username, g.site.hostname))
+    message='Subject: {}\n\n{}'.format(gettext("GNGforms. New user notification"), message)
+    
+    for email in adminEmails:
+        sendMail(email, message)
+
+
+def sendTestEmail(email):
+    message=gettext("Congratulations!")
+    message='Subject: {}\n\n{}'.format(gettext("SMTP test"), message)
+    
+    return sendMail(email, message)
---
+++ utils/wtf.py
@@ -0,0 +1,94 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, IntegerField, SelectField, PasswordField, BooleanField, RadioField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from flask import g
+from flask_babel import lazy_gettext as _
+
+from GNGforms import app
+from GNGforms.models import User, Installation
+from GNGforms.utils.utils import sanitizeUsername, pwd_policy
+
+
+class NewUser(FlaskForm):
+    username = StringField(_("Username"), validators=[DataRequired()])
+    email = StringField(_("Email"), validators=[DataRequired(), Email()])
+    password = PasswordField(_("Password"), validators=[DataRequired()])
+    password2 = PasswordField(_("Password again"), validators=[DataRequired(), EqualTo('password')])
+
+    def validate_username(self, username):
+        if username.data != sanitizeUsername(username.data):
+            raise ValidationError(_("Username is not valid"))
+            return False
+        if username.data in app.config['RESERVED_USERNAMES']:
+            raise ValidationError(_("Please use a different username"))
+            return False
+        if User.find(username=username.data):
+            raise ValidationError(_("Please use a different username"))
+
+    def validate_email(self, email):
+        if User.find(email=email.data):
+            raise ValidationError(_("Please use a different email address"))
+        elif email.data in app.config['ROOT_USERS'] and Installation.isUser(email.data):
+            # a root_user email can only be used once across all sites.
+            raise ValidationError(_("Please use a different email address"))
+            
+    def validate_password(self, password):
+        if pwd_policy.test(password.data):
+            raise ValidationError(_("Your password is weak"))
+            
+            
+class Login(FlaskForm):
+    username = StringField(_("Username"), validators=[DataRequired()])
+    password = PasswordField(_("Password"), validators=[DataRequired()])
+    
+    def validate_username(self, username):
+        if username.data != sanitizeUsername(username.data):
+            return False
+
+class GetEmail(FlaskForm):
+    email = StringField(_("Email address"), validators=[DataRequired(), Email()])
+    
+    
+class ChangeEmail(FlaskForm):
+    email = StringField(_("New email address"), validators=[DataRequired(), Email()])
+    
+    def validate_email(self, email):
+        if User.find(email=email.data):
+            raise ValidationError(_("Please use a different email address"))
+        elif email.data in app.config['ROOT_USERS'] and Installation.isUser(email.data):
+            # a root_user email can only be used once across all sites.
+            raise ValidationError(_("Please use a different email address"))
+
+
+class ResetPassword(FlaskForm):
+    password = PasswordField(_("Password"), validators=[DataRequired()])
+    password2 = PasswordField(_("Password again"), validators=[DataRequired(), EqualTo('password')])
+
+    def validate_password(self, password):
+        if pwd_policy.test(password.data):
+            raise ValidationError(_("Your password is weak"))
+
+
+class smtpConfig(FlaskForm):
+    host = StringField(_("Email server"), validators=[DataRequired()])
+    port = IntegerField(_("Port"))
+    encryption = SelectField(_("Encryption"), choices=[ ('None', 'None'),
+                                                        ('SSL', 'SSL'),
+                                                        ('STARTTLS', 'STARTTLS (maybe)')])
+    user = StringField(_("User"))
+    password = StringField(_("Password"))
+    noreplyAddress = StringField(_("Sender address"), validators=[DataRequired(), Email()])
+
+
+class NewInvite(FlaskForm):
+    email = StringField(_("New user's email"), validators=[DataRequired(), Email()])
+    message = TextAreaField(_("Include message"))
+    admin = BooleanField(_("Make the new user an Admin"))
+    hostname = StringField(_("hostname"), validators=[DataRequired()])
+    
+    def validate_email(self, email):
+        if User.find(email=email.data, hostname=self.hostname.data):
+            raise ValidationError(_("Please use a different email address"))
+        elif email.data in app.config['ROOT_USERS'] and Installation.isUser(email.data):
+            # a root_user email can only be used once across all sites.
+            raise ValidationError(_("Please use a different email address"))
