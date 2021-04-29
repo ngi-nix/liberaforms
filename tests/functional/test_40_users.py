1e43d941639165d89b5a41de0956741b69f43656
buttle <chris@gatopelao.org>
2021-04-29 15:50:48 +0200

    changes functional test order

--- tests/functional/test_40_users.py
+++ tests/functional/test_40_users.py
@@ -14,7 +14,7 @@ from liberaforms.models.user import User
 from tests.unit.conftest import dummy_user
 
 #@pytest.mark.usefixtures('users')
-@pytest.mark.order(after="TestAdmin")
+#@pytest.mark.order(after="TestAdmin")
 class TestUser():
 
     def test_save_new_user(self, db, dummy_user, users):
@@ -125,57 +125,3 @@ class TestUser():
         html = response.data.decode()
         assert '<div id="blurb" class="marked-up">' in html
         assert '<a class="nav-link" href="/user/login">' in html
-
-
-class TestAdmin():
-
-    def test_bootstrap_first_admin(self, db, users, client):
-        """ Creates the first admin user using ROOT_USER
-            as defined in ./tests/test.env
-        """
-        root_user_email = ast.literal_eval(os.environ['ROOT_USERS'])[0]
-        response = client.post(
-                        "/site/recover-password",
-                        data = {
-                            "email": root_user_email,
-                        },
-                        follow_redirects=False,
-                    )
-        assert response.status_code == 302
-        # this path contains the invite token
-        new_user_url = urlparse(response.location).path
-        username = root_user_email.split('@')[0]
-        password = users["admin_password"]
-        response = client.post(
-                            new_user_url,
-                            data = {
-                                "username": username,
-                                "email": root_user_email,
-                                "password": password,
-                                "password2": password,
-                            },
-                            follow_redirects=True,
-                        )
-        assert response.status_code == 200
-        html = response.data.decode()
-        assert "<!-- my_forms_page -->" in html
-        assert '<a class="nav-link" href="/user/logout">' in html
-        user = User.find(username=username)
-        assert user.admin['isAdmin'] == True
-        users['admin']=user
-
-    def test_toggle_new_user_notification(self, users, client):
-        notification = users['admin'].admin["notifyNewUser"]
-        response = client.post(
-                                "/admin/toggle-newuser-notification",
-                            )
-        assert response.status_code == 200
-        assert users['admin'].admin["notifyNewUser"] != notification
-
-    def test_toggle_new_form_notification(self, users, client):
-        notification = users['admin'].admin["notifyNewForm"]
-        response = client.post(
-                                "/admin/toggle-newform-notification",
-                            )
-        assert response.status_code == 200
-        assert users['admin'].admin["notifyNewForm"] != notification
