586920dc00912d087929e987969dc8e84935ffb3
Porrumentzio <porrumentzio@riseup.net>
2021-06-03 13:36:06 +0200

    Fixes error while compiling POT

--- liberaforms/views/form.py
+++ liberaforms/views/form.py
@@ -375,7 +375,8 @@ def add_shared_notification(id):
                 queriedForm.sharedNotifications = []
             if not wtform.email.data in queriedForm.sharedNotifications:
                 queriedForm.sharedNotifications.append(email)
-                queriedForm.add_log(gettext(f"Added shared notification: {email}"))
+                log_msg = _("Added shared notification: %s" % email)
+                queriedForm.add_log(log_msg)
                 queriedForm.save()
     return redirect(make_url_for('form_bp.share_form',
                                  id=queriedForm.id,
@@ -393,7 +394,7 @@ def remove_shared_notification(id):
         return JsonResponse(json.dumps(False))
     if email in queriedForm.sharedNotifications:
         queriedForm.sharedNotifications.remove(email)
-        queriedForm.add_log(gettext(f"Removed shared notification: {email}"))
+        queriedForm.add_log(_("Removed shared notification: %s" % email))
         queriedForm.save()
         return JsonResponse(json.dumps(True))
     return JsonResponse(json.dumps(False))
