c9774e69de1a6fac22a5e0c4f56af632994d34dc
Porrumentzio <porrumentzio@riseup.net>
2021-07-02 12:56:07 +0200

    Modify, fix and update dataTables translations (ca, es & eu)

--- liberaforms/static/dataTables-languages/es.js
+++ liberaforms/static/dataTables-languages/es.js
@@ -1,16 +1,15 @@
 {
-	"sProcessing":     "Procesando...",
-	"sLengthMenu":     "Mostrar _MENU_ registros",
-	"sZeroRecords":    "No se encontraron resultados",
 	"sEmptyTable":     "Ningún dato disponible en esta tabla",
 	"sInfo":           "Mostrando del _START_ al _END_ de _TOTAL_ registros",
-	"sInfoEmpty":      "Mostrando 0 al 0 de 0 registros",
+	"sInfoEmpty":      "Mostrando del 0 al 0 de 0 registros",
 	"sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
 	"sInfoPostFix":    "",
-	"sSearch":         "Buscar:",
-	"sUrl":            "",
 	"sInfoThousands":  ",",
+	"sLengthMenu":     "Mostrar _MENU_ registros",
 	"sLoadingRecords": "Cargando...",
+	"sProcessing":     "Procesando...",
+	"sSearch":         "Buscar:",
+	"sZeroRecords":    "No se encontraron resultados",
 	"oPaginate": {
 		"sFirst":    "Primero",
 		"sLast":     "Último",
@@ -18,7 +17,7 @@
 		"sPrevious": "Anterior"
 	},
 	"oAria": {
-		"sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
-		"sSortDescending": ": Activar para ordenar la columna de manera descendente"
+		"sSortAscending":  ": activar para ordenar la columna de manera ascendente",
+		"sSortDescending": ": activar para ordenar la columna de manera descendente"
 	}
 }
