448c2f4fbe7a2273f25cce00e39f1681befd3120
buttle <chris@gatopelao.org>
2021-07-03 12:05:34 +0200

    displays table rows as cards, per media width

--- liberaforms/static/data-table/data-table.js
+++ liberaforms/static/data-table/data-table.js
@@ -1,3 +1,5 @@
+/* Table pinning idea seen at http://foundation.zurb.com */
+/* Table card idea seen at https://www.exeideas.com/2020/10/simple-responsive-html-table.html */
 
 function HScrollVisible () {
   var table_width = 0;
@@ -5,89 +7,128 @@ function HScrollVisible () {
     if ($(table).width() > table_width) {
       table_width = $(table).width();
     }
-    console.log("table: "+table_width)
   });
   if (table_width > $(window).width()) {
-    return true;
+    return table_width;
   }
-  /*console.log("window: "+$(window).width())*/
   return false;
 }
 
 $(document).ready(function() {
   var switched = false;
-  var updateTables = function() {
-    //console.log(HScrollVisible())
-    if (!switched && HScrollVisible() == true) {
-      //console.log("splitting for Hscroll")
-      splitAll();
+
+  $(".show-card").on("click", function (){
+    if ($(this).hasClass('fa-chevron-circle-down')){
+      var tr = $(this).closest('tr')
+      $(tr).find('td').css('display', 'block')
+      $(tr).css('margin-bottom', '2em')
+      $(this).removeClass('fa-chevron-circle-down')
+             .addClass('fa-chevron-circle-up')
+    } else {
+      var tr = $(this).closest('tr')
+      $(tr).find('td').not(':first').css('display', '')
+      $(tr).css('margin-bottom', '')
+      $(this).removeClass('fa-chevron-circle-up')
+             .addClass('fa-chevron-circle-down')
+    }
+  });
+  //console.log(HScrollVisible())
+
+  function update_grid_tables () {
+    if ($(window).width() <= 600) {
+      if (switched) {
+        unsplit_all_grid_tables();
+      }
+      $("table.lb-data-table").each(function(i, element) {
+        $(element).css('opacity', 1);
+      });
+      return
+    }
+    if (!switched && HScrollVisible()) {
+      split_all_grid_tables();
     }
     else if (switched && HScrollVisible() == false) {
-      //console.log("un-splitting for Hscroll")
-      unsplitAll();
+      unsplit_all_grid_tables();
     }
     $("table.lb-data-table").each(function(i, element) {
       $(element).css('opacity', 1);
     });
+  }
 
-    /*
-    if (!switched && ($(window).width() < 767) ){
-      splitAll();
-    }
-    else if (switched && ($(window).width() > 767)) {
-      unsplitAll();
-    }
-    */
-  };
-  updateTables();
-  function splitAll() {
+  update_grid_tables();
+  function split_all_grid_tables() {
     switched = true;
     $("table.lb-data-table").each(function(i, element) {
-      splitTable($(element));
+      split_grid_table($(element));
+      console.log("splitting")
     });
     return true;
   }
-  function unsplitAll() {
+
+  function unsplit_all_grid_tables() {
     switched = false;
     $("table.lb-data-table").each(function(i, element) {
-      unsplitTable($(element));
+      usplit_grid_table($(element));
     });
   }
 
-  $(window).on("redraw",function(){switched=false; updateTables();}); // An event to listen for
-  $(window).on("resize", updateTables);
+  function cards_to_grid() {
+    if ($(window).width() >= 600 && $(".pinned-table").length == 0) {
+      console.log('card to grid')
+      $("table.lb-data-table").each(function(i, element) {
+        $(element).find('tr').each(function (i, tr) {
+          $(tr).find('td').css('display', '')
+          $(tr).css('margin-bottom', '')
+          $(tr).find('.fa-chevron-circle-up')
+               .removeClass('fa-chevron-circle-up')
+               .addClass('fa-chevron-circle-down')
+        });
+      });
+    }
+  }
 
-	function splitTable(original)
-	{
-    console.log('splitTable')
-		original.wrap("<div class='responsive-table' />");
+  function split_grid_table(original)
+  {
+    console.log('split_grid_table')
+    if (original.parent(".responsive-table").length == 0) {
+  	   original.wrap("<div class='responsive-table' />");
+    }
 
-		var copy = original.clone();
-		copy.removeClass("lb-data-table");
+  	var copy = original.clone();
+  	copy.removeClass("lb-data-table");
 
     var pinned_table = $("<table class='pinned-table' />")
     var thead_row = copy.find("thead").find("tr");
+    var tbody = $('<tbody />');
     var newRow = $("<tr></tr>");
     $(thead_row).find("th:first-child").clone().appendTo(newRow);
     $(thead_row).find("th:nth-child(2)").clone().appendTo(newRow);
     $(pinned_table).append($('<thead />').append(newRow));
+    $(pinned_table).append(tbody);
     copy.find("tbody").find("tr").each(function(index) {
         var newRow = $("<tr></tr>");
         $(this).find("td:first-child").clone().appendTo(newRow);
         $(this).find("td:nth-child(2)").clone().appendTo(newRow);
-        $(pinned_table).append($('<tbody />').append(newRow));
+        $(tbody).append(newRow);
     });
     //console.log(pinned_table)
     original.addClass('hidden-for-pin');
-		original.closest(".responsive-table").append(pinned_table);
-		original.wrap("<div class='scrollable' />");
-	}
+  	original.closest(".responsive-table").append(pinned_table);
+  	original.wrap("<div class='scrollable' />");
+  }
 
-	function unsplitTable(original) {
+  function usplit_grid_table(original) {
     //console.log("unsplit table")
     original.removeClass('hidden-for-pin');
     original.closest(".responsive-table").find(".pinned-table").remove();
     original.unwrap('.scrollable');
-	}
+  }
+
+  $(window).on("redraw",function(){switched=false; update_grid_tables();}); // An event to listen for
+  $(window).on("resize", function(){ cards_to_grid(); update_grid_tables();});
+
+});
+
+$(document).ready(function() {
 
 });
