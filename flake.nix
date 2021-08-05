1cb7832b1a23ce030ad60a521f12719040ee7bbf
Cleeyv <cleeyv@riseup.net>
2021-08-05 12:27:26 -0400

    WIP on flake dev, debugging flask cffi error

---
+++ flake.nix
@@ -0,0 +1,157 @@
+{
+
+  # This file is adapted from https://github.com/ngi-nix/vulnerablecode/blob/main/etc/nix/flake.nix
+
+  description =
+    "Liberaforms — An open source form server";
+
+  inputs.nixpkgs = {
+    type = "github";
+    owner = "NixOS";
+    repo = "nixpkgs";
+    ref = "nixos-unstable";
+  };
+
+  inputs.machnix = {
+    type = "github";
+    owner = "DavHau";
+    repo = "mach-nix";
+    ref = "3.3.0";
+  };
+
+  outputs = { self, nixpkgs, machnix }:
+    let
+
+      liberaforms-src = ./.;
+
+      # Extract version from VERSION.txt.
+      remove-newline = string: builtins.replaceStrings [ "\n" ] [ "" ] string;
+      version = remove-newline (builtins.readFile (liberaforms-src + "/VERSION.txt"));
+
+      # Common shell code.
+      # libSh = ./lib.sh;
+
+      # System types to support.
+      supportedSystems = [ "x86_64-linux" ];
+
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = f:
+        nixpkgs.lib.genAttrs supportedSystems (system: f system);
+
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (system:
+        import nixpkgs {
+          inherit system;
+          overlays = [ self.overlay ];
+        });
+
+      # mach-nix instantiated for supported system types.
+      machnixFor = forAllSystems (system:
+        import machnix {
+          pkgs = (nixpkgsFor.${system}).pkgs;
+          python = "python38";
+
+          # Pin pypi repo to a specific commit which includes all necessary
+          # Python deps. The default version is updated with every mach-nix
+          # release might be be sufficient for newer releases.
+          # The corresponding sha256 hash can be obtained with:
+          # $ nix-prefetch-url --unpack https://github.com/DavHau/pypi-deps-db/tarball/<pypiDataRev>
+          # pypiDataRev = "c86b4490a7d838bd54a2d82730455e96c6e4eb14";
+          # pypiDataSha256 =
+          #  "0al490gi0qda1nkb9289z2msgpc633rv5hn3w5qihkl1rh88dmjd";
+        });
+
+    in {
+
+      # A Nixpkgs overlay.
+      overlay = final: prev:
+        with final.pkgs; {
+
+          pythonEnv = machnixFor.${system}.mkPython {
+            requirements =
+              builtins.readFile (liberaforms-src + "/requirements.txt");
+          };
+
+          liberaforms = stdenv.mkDerivation {
+            inherit version;
+            name = "liberaforms-${version}";
+            src = liberaforms-src;
+            dontConfigure = true; # do not use ./configure
+            propagatedBuildInputs = [ pythonEnv ];
+
+            postPatch = ''
+              # Make sure the pycodestyle binary in $PATH is used.
+              # substituteInPlace tests/functional/*.py \
+              #  --replace 'join(bin_dir, "pycodestyle")' '"pycodestyle"'
+            '';
+
+            installPhase = ''
+              cp -r . $out
+            '';
+          };
+
+        };
+
+      # Provide a nix-shell env to work with liberaforms.
+      devShell = forAllSystems (system:
+        with nixpkgsFor.${system};
+        mkShell {
+          # will be available as env var in `nix develop` / `nix-shell`.
+          LIBERAFORMS_INSTALL_DIR = liberaforms;
+          buildInputs = [ liberaforms ];
+          #shellHook = ''
+          #  alias vulnerablecode-manage.py=${vulnerablecode}/manage.py
+          #'';
+        });
+
+      # Provide some packages for selected system types.
+      packages = forAllSystems
+        (system: { inherit (nixpkgsFor.${system}) liberaforms; });
+
+      # The default package for 'nix build'.
+      defaultPackage =
+        forAllSystems (system: self.packages.${system}.liberaforms);
+
+      # Tests run by 'nix flake check' and by Hydra.
+      #      checks = forAllSystems
+      #        (system: {
+      #          inherit (self.packages.${system}) liberaforms;
+      #
+      #  liberaforms-test = with nixpkgsFor.${system};
+      #    stdenv.mkDerivation {
+      #      name = "${liberaforms.name}-test";
+      #
+      #      buildInputs = [ wget liberaforms ];
+      #
+      #      # Used by pygit2.
+      #      # See https://github.com/NixOS/nixpkgs/pull/72544#issuecomment-582674047.
+      #      SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
+      #
+      #      unpackPhase = "true";
+      #
+      #      buildPhase = ''
+      #        source ${libSh}
+      #        initPostgres $(pwd)
+      #        export DJANGO_DEV=1
+      #        ${vulnerablecode}/manage.py migrate
+      #      '';
+      #
+      #      doCheck = true;
+      #      checkPhase = ''
+      #        # Run pytest on the installed version. A running postgres
+      #        # database server is needed.
+      #        (cd ${vulnerablecode} && pytest)
+      #
+      #         # Launch the webserver and call the API.
+      #         ${vulnerablecode}/manage.py runserver &
+      #        sleep 2
+      #        wget http://127.0.0.1:8000/api/
+      #        kill %1 # kill background task (i.e. webserver)
+      #      '';
+      #
+      #        installPhase =
+      #        "mkdir -p $out"; # make this derivation return success
+      #    };
+      #});
+    };
+}
