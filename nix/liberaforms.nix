b99b98622453c88bc1dd6b8ceb8febfbb9a2e412
Cleeyv <cleeyv@riseup.net>
2021-08-10 13:13:59 -0400

    Added nginx config, fixed supervisor service, added comments

--- nix/liberaforms.nix
+++ nix/liberaforms.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }:
+{ config, pkgs, lib, ... }:
 {
 
   ## Postgres database ##
@@ -14,13 +14,14 @@
   '';
 
 
-## Supervisor process manager ##
+  ## Supervisor process manager ##
 
   environment.systemPackages = with pkgs; [ python38Packages.supervisor ];
 
   # From https://github.com/Supervisor/supervisor/blob/master/supervisor/skel/sample.conf
   environment.etc."supervisor/supervisord.conf".source = "/home/cleeyv/dev/liberaforms/nix/supervisord.conf";
 
+  # Based on https://github.com/Supervisor/initscripts/blob/master/centos-systemd-etcs
   systemd.services.supervisor = {
     enable = true;
     description = "supervisord - Supervisor process control system for UNIX";
@@ -28,18 +29,61 @@
 
     serviceConfig = {
       Type = "forking";
-      # Unclear where supervisord.conf should be located.
       ExecStart = "/run/current-system/sw/bin/supervisord -c /etc/supervisor/supervisord.conf";
-      ExecReload = "/run/current-system/sw/bin/supervisord reload";
-      ExecStop = "/run/current-system/sw/bin/supervisord shutdown";
+      ExecReload = "/run/current-system/sw/bin/supervisorctl reload";
+      ExecStop = "/run/current-system/sw/bin/supervisorctl shutdown";
+      Restart = "on-failure";
+      RestartSec = "42s";
     };
-    # Added so that the service will start automatically.
-    # Possibly also add a "Restart" to the serviceConfig if it doesn't recover from failures.
     wantedBy = [ "multi-user.target" ];
   };
 
 
   ## Nginx reverse proxy ##
 
+  # Based on https://gitlab.com/liberaforms/liberaforms/-/blob/main/docs/nginx.example
+
+  networking.extraHosts =
+    ''
+      127.0.0.1 liberaforms
+      127.0.0.1 forms.cleeyv.tech
+    '';
+
+  # HTTPS will remain disabled until testing on a public host
+  services.nginx.enable = true;
+  # security.acme.acceptTerms = true;
+  # security.acme.email = "cleeyv@riseup.net";
+
+  services.nginx.virtualHosts."forms.cleeyv.tech" = {
+    listen = [{
+      addr = "127.0.0.1";
+      port = 80;
+    }];
+    # enableACME = true;
+    # forceSSL = true;
+    extraConfig = ''
+      access_log /var/log/nginx/liberaforms.access.log;
+      error_log /var/log/nginx/liberaforms.error.log notice;
+    '';
+  };
+
+  services.nginx.virtualHosts."forms.cleeyv.tech".locations."/" = {
+    # Aliases for static, favicon, logo, emailheader, and media paths could be added here later
+    proxyPass = "http://liberaforms:5000";
+    extraConfig = ''
+      proxy_set_header    Host    $host;
+      proxy_set_header    X-Forwarded-For $remote_addr;
+      proxy_set_header    X-Real-IP   $remote_addr;
+      proxy_pass_header   server;
+      proxy_set_header Host $host;
+    '';
+  };
+
+  # Send nginx error and access logs to journald
+  services.nginx.appendHttpConfig = ''
+    error_log stderr;
+    access_log syslog:server=unix:/dev/log combined;
+    types_hash_bucket_size 128; # Clean warning from nginx log
+  '';
 
 }
