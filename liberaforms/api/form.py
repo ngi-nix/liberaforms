4b442c1d9f44a0de942a71accd857d6000554cea
buttle <chris@gatopelao.org>
2021-08-24 00:42:35 +0200

    adds forms endpoint for data-table

--- liberaforms/api/form.py
+++ liberaforms/api/form.py
@@ -12,23 +12,67 @@ from liberaforms.models.schemas.form import FormSchema
 from liberaforms.models.schemas.answer import AnswerSchema
 from liberaforms.utils.wraps import *
 
+from pprint import pprint
 
 form_api_bp = Blueprint('form_api_bp', __name__)
 
 
-@form_api_bp.route('/api/forms', methods=['GET'])
+@form_api_bp.route('/api/forms/<int:user_id>', methods=['GET'])
 @enabled_user_required__json
-def all_forms():
+def my_forms(user_id):
+    """ Return json required by vue data-table component
+    """
+    if not user_id == g.current_user.id:
+        return jsonify("Denied"), 401
     forms = Form.find_all(editor_id=g.current_user.id)
+    form_count = forms.count()
+    forms = FormSchema(many=True).dump(forms)
+    field_index = [ {'name': 'form__html', 'label': _('Form')},
+                    {'name': 'answers__html', 'label': _('Answers')},
+                    {'name': 'created', 'label': _('Created')}
+                ]
+    items = []
+    for form in forms:
+        item = {}
+        data = {}
+        id = form['id']
+        url = form['url']
+        slug = form['slug']
+        total_answers = form['total_answers']
+        data['form__html'] = f"<a href='/forms/view/{id}'>{slug}</a>"
+        icon =f"<i class='fa fa-bar-chart' aria-label=\"{_('Statistics')}\"></i>"
+        stats_link = f"<a href='/forms/answers/stats/{id}'>{icon}</a>"
+        answers_link = f"<a href='/forms/answers/{id}' \
+                            alt_text=\"{_('Answers')}\"> \
+                            {total_answers} \
+                            </a>"
+        data['answers__html'] = f"{stats_link} {answers_link}"
+
+        for key in form.keys():
+            if key == 'slug' or key == 'url' or key == 'total_answers':
+                continue
+            if key == 'id':
+                item[key] = form[key]
+                continue;
+            if key == 'created':
+                item[key] = form[key]
+                continue;
+            data[key] = form[key]
+        item['data'] = data
+        items.append(item)
+        #pprint(items)
+
     return jsonify(
-        items=FormSchema(many=True).dump(forms),
-        meta={'count': forms.count()}
+        items=items,
+        meta={'total': form_count, 'field_index': field_index}
     ), 200
 
 
 @form_api_bp.route('/api/form/<int:form_id>/answers', methods=['GET'])
 @enabled_user_required__json
 def form_answers(form_id):
+    """ Return json required by vue data-table component
+    """
     form = Form.find(id=form_id, editor_id=g.current_user.id)
     if not form:
         return jsonify("Denied"), 401
--- liberaforms/models/schemas/form.py
+++ liberaforms/models/schemas/form.py
@@ -17,11 +17,15 @@ class FormSchema(ma.SQLAlchemySchema):
     created = ma.auto_field()
     slug = ma.auto_field()
     url = ma.Method('get_url')
+    total_answers = ma.Method('get_total_answers')
     structure = ma.auto_field()
     introduction_md = ma.Method('get_introduction_md')
 
     def get_url(self, obj):
         return obj.url
+
+    def get_total_answers(self, obj):
+        return obj.answers.count()
 
     def get_introduction_md(self, obj):
         return obj.introductionText['markdown']
