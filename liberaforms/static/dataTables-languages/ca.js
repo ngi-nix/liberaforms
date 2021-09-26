c1bf8471533e4819d90b0b68c7ebe5b46de99818
buttle <chris@gatopelao.org>
2021-09-26 21:20:28 +0200

    Recovers deleted library that is used to generate the form log table

---
+++ liberaforms/static/dataTables-languages/ca.js
@@ -0,0 +1,24 @@
+{
+	"sEmptyTable":   "No hi ha dades disponibles a la taula"
+	"sInfo":         "Mostrant de _START_ a _END_ de _TOTAL_ registres",
+	"sInfoEmpty":    "Mostrant de 0 a 0 de 0 registres",
+	"sInfoFiltered": "(filtrat de _MAX_ total registres)",
+	"sInfoPostFix":  "",
+	"sInfoThousands":  ",",
+	"sLengthMenu":   "Mostra _MENU_ registres",
+	"sLoadingRecords": "Carregant...",
+	"sProcessing":   "Processant...",
+	"sSearch":       "Filtrar:",
+	"sZeroRecords":  "No s'han trobat registres.",
+	"sSearch":       "Filtrar:",
+	"oPaginate": {
+		"sFirst":    "Primer",
+		"sPrevious": "Anterior",
+		"sNext":     "Següent",
+		"sLast":     "Últim"
+	},
+	"oAria": {
+		"sSortAscending":  ": activa per ordenar la columna en sentit ascendent",
+		"sSortDescending": ": activa per ordenar la columna en sentit descendent"
+	}
+}
---
+++ liberaforms/static/dataTables-languages/eu.js
@@ -0,0 +1,23 @@
+{
+	"sEmptyTable":     "Ez dago datu erabilgarririk taulan",
+	"sInfo":           "_START_(e)tik _END_(e)ra erakusten, guztirazko _TOTAL_ erantzunetatik",
+	"sInfoEmpty":      "0-0 erakusten, guztirazko 0 erantzunetatik",
+	"sInfoFiltered":   "(guztirazko _MAX_ erantzunetatik iragazita)",
+	"sInfoPostFix":    "",
+	"sInfoThousands":  ",",
+	"sLengthMenu":     "Erakutsi _MENU_ erantzun",
+	"sLoadingRecords": "Kargatzen...",
+	"sProcessing":     "Prozesatzen...",
+	"sSearch":         "Bilatu:",
+	"sZeroRecords":    "Ez da aurkitu bat datorrenik",
+	"oPaginate": {
+		"sFirst":    "Lehena",
+		"sLast":     "Azkena",
+		"sNext":     "Hurrengoa",
+		"sPrevious": "Aurrekoa"
+	},
+	"oAria": {
+		"sSortAscending":  ": gaitu zutabeak gorantz ordenatzeko",
+		"sSortDescending": ": gaitu zutabeak beherantz ordenatzeko"
+	}
+}
